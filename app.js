0191ab2432032569abb2fc7567cbbccf45c5ca0e
Brian Schorn <schorn.brian@gmail.com>
2017-01-17 16:51:37 -0600

    Fixed space after $

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var print = require('./exporter');
 http.createServer(function(req, res) {
   res.writeHead(200); //sets the response code in the header
   // res.write("test \n");
-  res.write(print.printer());
-  res.write(print.randomizer());
+  res.write(print.printer() + print.randomizer() );
 
   // res.write(print.printer() + print.random()); //inserts text into the response
 
